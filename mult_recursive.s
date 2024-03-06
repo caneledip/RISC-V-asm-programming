04af4d5c2906faa2e151fa53e3c74a06d7c351b3
Tanakorn Kullapatchawarit <92836331+caneledip@users.noreply.github.com>
2024-03-06 23:18:19 +0700

    Create mult_recursive.s

---
+++ mult_recursive.s
@@ -0,0 +1,48 @@
+.text
+main:
+    # pass the first argument to a0
+    # pass the second argument to a1
+    addi a0, x0, 120
+    addi a1, x0, 60
+    jal mult
+    
+    # print_int
+    mv a1, a0 # by convention the return value is always in a0
+    addi a0, x0, 1
+    ecall
+
+    # exit cleanly
+    addi a0, x0, 10
+    ecall
+    
+mult:
+    # base case
+    # compare a1 with 1, if the two are equal you exit the mult function
+    addi t0, x0, 1
+    beq a1, t0, exit_base_case
+    
+    # recursive case
+    addi sp, sp, -4
+    sw ra, 0(sp) # storing the ra value on to the stack
+    
+    # mult(a, b-1);
+    
+    addi sp, sp, -4
+    sw a0, 0(sp)
+    addi a1, a1, -1
+    jal mult
+    
+    # a + mult(a, b-1)
+    # mv t1, a0
+    
+    # restore the original a value before the call to mult
+    lw t1, 0(sp)
+    addi sp, sp, 4
+    add a0, a0, t1
+    
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    jr ra
+    
+exit_base_case:
+    jr ra
